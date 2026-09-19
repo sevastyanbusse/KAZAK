3a2dc6e288f6c3fb7d7fc32481bc74f82f5ba821
Edward <sevastyanbusse@proton.me>
2026-09-19 10:52:45 -0700

    Update agentic-ai.py

--- core/agentic-ai.py
+++ core/agentic-ai.py
@@ -1,1 +1,6 @@
-# The code from agentic-ai will be moved here.
+#Dependencies need to be installed to run lang-chain
+
+import os 
+from dotenv import load_dotenv
+from langchain.agents import create_openai_functions_agent, AgentExecutor
+from langchain.tools import DuckDuckGoSearchRun
